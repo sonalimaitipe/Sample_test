05ca9ce6a8fc603b05a7e75043ae00e9ff708e82
Maitipe <smaitipe@wiley.com>
2021-02-15 15:28:14 +0530

    Test complted for Login

---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,23 @@
+package login;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.Assert.*;
+
+public class LoginTests extends BaseTest {
+    @Test
+    public void testSuccessfullLogin () {
+        LoginPage loginPage =  homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        assertTrue(secureAreaPage.getAlerttext().contains("You logged into a secure area"),"Alert text icorrect");
+
+
+
+
+    }
+}
